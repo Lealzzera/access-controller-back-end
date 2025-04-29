3af0b58e01848fbd5444bdfd6b015e464426e8a1
Matheus Leal <matheus.mleall@gmail.com>
2025-04-29 13:22:34 -0300

    adjusted controller to get the presigned url to fetch images from S3 bucket

--- src/modules/download/download.controller.ts
+++ src/modules/download/download.controller.ts
@@ -1,15 +1,21 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { generateDownloadURL } from 'src/aws/generate-download-url';
 import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('download')
 @UseGuards(AuthGuard)
 export class DownLoadController {
-  @Get('generate-presigned-url')
-  async getPresignedUrl(@Query('fileName') fileName: string) {
+  @Post('generate-presigned-url')
+  async getPresignedUrl(@Body() body: { files: string[] }) {
     try {
-      const url = await generateDownloadURL(fileName);
-      return { url };
+      const getPresignedUrlList = await Promise.all(
+        body.files.map(async (fileName) => {
+          const url = await generateDownloadURL(fileName);
+          return { fileName, url };
+        }),
+      );
+
+      return { urls: getPresignedUrlList };
     } catch (err) {
       return err.response;
     }
