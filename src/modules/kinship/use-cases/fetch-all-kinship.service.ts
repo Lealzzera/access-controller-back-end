14193d91255134a26025081ae42cd665168d6b8d
Matheus Leal <matheus.mleall@gmail.com>
2025-05-06 17:31:42 -0300

    created an usecase to fetch all kinship list

---
+++ src/modules/kinship/use-cases/fetch-all-kinship.service.ts
@@ -0,0 +1,20 @@
+import { Inject, Injectable } from '@nestjs/common';
+import IKinshipRepository from '../repositories/interfaces/kinship-repository.interface';
+import { Kinship } from '@prisma/client';
+
+interface FetchAllKinshipServiceResponse {
+  kinships: Kinship[];
+}
+
+@Injectable()
+export default class FetchAllKinshipService {
+  constructor(
+    @Inject('IKinshipRepository')
+    private readonly kinshipRepository: IKinshipRepository,
+  ) {}
+
+  async exec(): Promise<FetchAllKinshipServiceResponse> {
+    const kinships = await this.kinshipRepository.fetchAllKinship();
+    return { kinships };
+  }
+}
